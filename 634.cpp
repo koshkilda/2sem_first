d66aa0c1c0755c642e8ab87ec3a6a4bb8c747ecb
koshkilda <kotishka2014@gmail.com>
2023-04-04 15:32:06 +0300

    Add some comments

--- 634.cpp
+++ 634.cpp
@@ -5,30 +5,30 @@ using namespace std;
 
 int main(){
     // наверное, это надо было как-то по-другому делать...
-    double n;
+    double n; // ввод первого числа
     cin >> n;
-    int num_of_operations;
+    int num_of_operations; // ввод количества операций
     cin >> num_of_operations;
 
     if (num_of_operations != 0) {
-        const int max = 20;
+        const int max = 20; // вывод открывающих скобок при количестве операций != 0
         string operations[max];
         for (int i = 0; i < num_of_operations; i++) {
             cin >> operations[i];
         }
-        
+
         int k = 0;
         while (k < num_of_operations) {
-            cout << "("; 
+            cout << "(";
             k++;
         }
-        
-        cout << n;
-        
-        for (int i = 0; i < num_of_operations; i++) {
+
+        cout << n; // вывод самого числа при количестве операций != 0
+
+        for (int i = 0; i < num_of_operations; i++) { // вывод закрывающих скобок и операций при количестве операций != 0
             cout << ")" << operations[i];
         }
-    } else cout << n;
+    } else cout << n; // вывод числа при количестве операций == 0
 
     return 0;
 }
