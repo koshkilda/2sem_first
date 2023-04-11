b7b9172dcfea47887ff978af5ac5f32ba895861a
koshkilda <kotishka2014@gmail.com>
2023-04-11 19:56:48 +0300

    Changed a lot

--- 634.cpp
+++ 634.cpp
@@ -1,34 +1,37 @@
 #include <iostream>
 #include <string>
+#include <deque>
 
 using namespace std;
 
 int main(){
-    // наверное, это надо было как-то по-другому делать...
-    double n; // ввод первого числа
-    cin >> n;
-    int num_of_operations; // ввод количества операций
-    cin >> num_of_operations;
+    deque <string> d; //создание deqa
 
-    if (num_of_operations != 0) {
-        const int max = 20; // вывод открывающих скобок при количестве операций != 0
-        string operations[max];
-        for (int i = 0; i < num_of_operations; i++) {
-            cin >> operations[i];
-        }
+    double a = 0;
+    int num_of_operations = 0;
+    cin >> a >> num_of_operations; //ввод первого числа и количества операций
 
-        int k = 0;
-        while (k < num_of_operations) {
-            cout << "(";
-            k++;
-        }
+    char operation = 0;
+    char num  = 0;
+    for (int i = 0; i < num_of_operations; i++)
+    {
+        cin >> operation >> num; // ввод опреаций
 
-        cout << n; // вывод самого числа при количестве операций != 0
+        d.push_back(operation); // добавление операций в deq
+        d.push_back(num);
 
-        for (int i = 0; i < num_of_operations; i++) { // вывод закрывающих скобок и операций при количестве операций != 0
-            cout << ")" << operations[i];
-        }
-    } else cout << n; // вывод числа при количестве операций == 0
+        cout << "("; // вывод открывающих скобочек
+    }
+
+    cout << a; // вывод первого числа
+
+    for (int i = 0; i < num_of_operations; i++)
+    {
+        d.pop_front(&operation); // чистим deq
+        d.pop_front(&num);
+
+        cout << ")" << operation << num; // вывод операций
+    }
 
     return 0;
 }
